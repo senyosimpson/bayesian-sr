e208dd6e4fe67bb6615e9c0eb7e06bb6190bd145
Senyo <simpsonsenyo@gmail.com>
2019-09-17 23:40:31 +0200

    update

--- resolve.py
+++ resolve.py
@@ -1,19 +1,9 @@
-# Define functions to calculate all the various matrices
-# Most of the calculations are done per image which means no multidimensional problem formulations
-# For the W matrix, every pixel ji is normalized with the sum of all pixels in that row
-
 import numpy as np
 
 
 def cov(x_i, x_j, r=1.0, a=1.0):
-    """ The covariance function that determines smoothness of GP
-    It encodes some similarity about points near each other
-    x_i and x_j are the indexes to those points, hence they are
-    technically the same vector and the logic for batch calculations
-    is done here instead of separate indexing of i,j coordinates
-    x_i has shape m x d
-    x_j  has shape n x d
-    out has shape m x n (cov matrix between all points)
+    """ 
+    The covariance function that determines smoothness of GP
     """
     x_i_sum = np.sum(x_i**2, axis=1).reshape(-1, 1)
     x_j_sum = np.sum(x_j**2, axis=1)
@@ -22,53 +12,56 @@ def cov(x_i, x_j, r=1.0, a=1.0):
     return out
 
 
-def transform_mat(x_i, x_j, center, shifts, gamma):
+def transform_mat(x_i, x_j, center, shift, angle, gamma=2):
     """ This is the transformation matrix that converts a high
     resolution image into a low resolution image
     In the paper this is denoted using W_ji
-    Remember normalization
     """
     x_i_sum = np.sum(x_i**2, axis=1).reshape(-1, 1)
-    u_j = psf_center(x_j, center, shifts)
+    u_j = psf_center(x_j, center, shift, angle)
     u_j_sum = np.sum(u_j**2, axis=1)
     dist = x_i_sum + u_j_sum - (2 * np.dot(x_i, u_j.T))
     out = np.exp(-(dist / gamma**2))
-    # below could be incorrect, remember to change to axis=1 if results are poor
     out /= np.sum(out, axis=0)
     return out
 
 
-def psf_center(x_j, center, shifts, angle=None):
+def psf_center(x_j, center, shift, angle):
     """ Used to calculate the center of the psf
     This is the vector u_j in the paper
-
-    shifts is a vector with shape (x,)
-
-    In the 1D case, we leave out the rotation
     """
+    center = center.reshape(-1, 2)
+    shift = shift.reshape(-1, 2)
     rotation_matrix = [
         [np.cos(angle), np.sin(angle)],
         [-np.sin(angle), np.cos(angle)]]
-    #u = np.dot(rotation_matrix, (x_j - center).T) + center + shifts
-    u = x_j + shifts
-    # in the 2d case, u = center + shifts.reshape(-1,2)
+    if isinstance(angle, np.ndarray):
+        rotation_matrix = []
+        for a in angle:
+            rotation_matrix.append([
+                [np.cos(a), np.sin(a)],
+                [-np.sin(a), np.cos(a)]])
+        rotation_matrix = np.array(rotation_matrix)
+
+    u = np.dot(rotation_matrix, (x_j - center).T)
+    u = u.T + center + shift
     return u
 
 
-def variance(Z_x, W_vec, W, beta):
+def variance(Z_x, W_K, W_ji, beta):
     Z_x_inv = np.linalg.inv(Z_x)
     #d = 0
     #for k in range(len(W)):
     #    d += np.dot(W_vec.T, W[k])
-    a = np.dot(W_vec, W)
+    a = np.dot(W_K.T, W_ji)
     a = np.sum(a, axis=0)
     sigma = Z_x_inv + (beta * a)
     return np.linalg.inv(sigma)
 
 
-def mean(WK, y, beta, sigma):
+def mean(W_K, y, beta, sigma):
     """
-    :param WK:
+    :param W_K:
     :param y: a kxM dimension matrix
     :param beta:
     :param sigma:
@@ -77,7 +70,7 @@ def mean(WK, y, beta, sigma):
     #a = 0
     #for k in range(len(y)):
     #    a += np.dot(WK.T, y[k])
-    a = np.dot(WK.T, y)
+    a = np.dot(W_K.T, y)
     a = np.sum(a, axis=0)
     mu = beta * np.matmul(sigma, a)
     return mu
@@ -99,7 +92,7 @@ def marginal_log_likelihood(beta, y, W, mu, Z_x, sigma, K, M):
     return likelihood
 
 
-def compute_likelihood():
+# gamma is a learnable parameter
+def compute_likelihood(X, y, center, shifts, gamma):
     # takes in the necessary parameters to calculate everything and return a likelihood score
     pass
-
